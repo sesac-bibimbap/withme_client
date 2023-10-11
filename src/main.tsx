1040d86d1d546a8a57d9b17aeb7f20f31d9a8b91
SNXWXH <snxwxh@gmail.com>
2023-10-11 15:25:00 +0900

    🚚 chore: main, App 주석 처리

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import '../src/global.style.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-const queryClient = new QueryClient();
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+// import '../src/global.style.css';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <QueryClientProvider client={queryClient}>
+//       <Router>
+//         <App />
+//       </Router>
+//       <ReactQueryDevtools initialIsOpen={false} />
+//     </QueryClientProvider>
+//   </React.StrictMode>,
+// );
