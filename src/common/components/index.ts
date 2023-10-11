bc2a69d31b1d25d0c0a12ad378f65199a335785e
SNXWXH <snxwxh@gmail.com>
2023-10-12 01:05:43 +0900

    💄 design: 버튼 export
    charcoalBtn index에 추가

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -3,3 +3,4 @@ export { default as PasswdInput } from './passwdInput/PasswdInput';
 export { default as Popup } from './popup/Popup';
 export { default as YellowBtn } from './yellowBtn/YellowBtn';
 export { default as TechStackHashtag } from './techStackHashtag/TechStackHashtag';
+export { default as CharcoalBtn } from './charcoalBtn/CharcoalBtn';
