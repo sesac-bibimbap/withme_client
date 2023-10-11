e18267654cb049104768b9650b4f717209744e50
SNXWXH <snxwxh@gmail.com>
2023-10-11 15:19:50 +0900

    🚚 chore: front 초기 설정
    axios, antDesign, antDesign icon, react router dom, react query, react query devtools
    폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-// import PAGE_LIST from './pages';
-// import { useRoutes } from 'react-router-dom';
+import PAGE_LIST from './pages';
+import { useRoutes } from 'react-router-dom';
 
 const App = () => {
-  // const routes = useRoutes(PAGE_LIST);
-  // return routes;
-  return (
-    <>
-      <div>하잉이ㄴㅇㄴㄴ</div>
-    </>
-  );
+  const routes = useRoutes(PAGE_LIST);
+
+  return routes;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '../src/global.style.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
