0003bd65fba5d72aebe921b1b9470bfffbcde07f
SNXWXH <snxwxh@gmail.com>
2023-10-18 20:42:46 +0900

    💄 design: studyPage CSS 수정

--- src/pages/study/list/StudyPage.tsx
+++ src/pages/study/list/StudyPage.tsx
@@ -1,10 +1,12 @@
 import { StudyBookmark, StudyListBoard } from '../../../components';
+import { studyPage_right_wrapper } from './StudyPage.style';
 
 const StudyPage = () => {
   return (
     <>
       <StudyListBoard />
-      <div style={{ height: '100%' }}>
+      <div style={studyPage_right_wrapper}>
+        {/* <StudyBookmark bookmarkStyle={{ height: '100%' }} /> */}
         <StudyBookmark />
         <StudyBookmark />
       </div>
