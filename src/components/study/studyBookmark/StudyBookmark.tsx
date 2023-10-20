716a163552539b4971fb011a8cbcc0210a775f66
SNXWXH <snxwxh@gmail.com>
2023-10-20 17:31:52 +0900

    ✨ feat: react-query cache 사용하여 북마크 목록 렌더링

--- src/components/study/studyBookmark/StudyBookmark.tsx
+++ src/components/study/studyBookmark/StudyBookmark.tsx
@@ -1,38 +1,33 @@
 import { CSSProperties } from 'react';
-import { useProfileQuery } from '../../auth/hooks/queries/useQueries';
 import {
   studyBookmark_item_scroll,
   studyBookmark_text_title,
   studyBookmark_wrapper,
 } from './StudyBookmark.style';
 import StudyBookmarkItem from './studyBookmarkItem/StudyBookmarkItem';
+import useCacheInstance from '../../../common/utils/cache';
 
 type StudyBookmarkType = {
   bookmarkStyle?: CSSProperties;
 };
 
 const StudyBookmark = ({ bookmarkStyle }: StudyBookmarkType) => {
-  const { data, isLoading } = useProfileQuery();
-
-  let bookmarkedStudies = [];
-  if (!isLoading) bookmarkedStudies = data.bookmarkedStudies;
-
+  const { cache } = useCacheInstance();
+  const user = cache.getQueryData(['login-user']);
+  const bookmarkedStudies = user.bookmarkedStudies || [];
   const bookmarkCss = {
     ...studyBookmark_wrapper,
     ...bookmarkStyle,
   };
+
   return (
     <>
       <div style={bookmarkCss}>
         <p style={studyBookmark_text_title}>북마크</p>
         <div style={studyBookmark_item_scroll}>
           {bookmarkedStudies.length !== 0 ? (
-            bookmarkedStudies?.map((bookmark: Bookmark) => (
-              <StudyBookmarkItem
-                key={bookmark.id}
-                id={bookmark.id}
-                teamName={bookmark.name}
-              />
+            bookmarkedStudies?.map(({ id, name }: Bookmark) => (
+              <StudyBookmarkItem key={id} id={id} teamName={name} />
             ))
           ) : (
             <p>원하는 스터디를 북마크하세요</p>
